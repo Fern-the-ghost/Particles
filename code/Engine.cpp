33709d7f412f4a014d32f475783af9c879d08371
Fern-the-ghost <W1907532@apps.losrios.edu>
2025-05-04 17:38:50 -0700

    Update Engine.cpp

--- code/Engine.cpp
+++ code/Engine.cpp
@@ -1,6 +1,8 @@
 #include "Engine.h"
 #include <SFML/Graphics.hpp>
 #include "Particle.h"
+#include <SFML/System/Clock.hpp>
+#include <SFML/System/Time.hpp>
 using namespace sf;
 using namespace std;
 
@@ -15,6 +17,7 @@ Engine::Engine()
 
 void input()
 {
+  
 }
 
 void update(float dtAsSeconds)
@@ -23,8 +26,24 @@ void update(float dtAsSeconds)
 
 void draw()
 {
+  
 }
 
 void run()
 {
+  Clock clock;
+  Time time1 = clock.getElapsedTime();
+  
+  cout << "Starting Particle unit tests..." << endl;
+  Particle p(m_Window, 4, { (int)m_Window.getSize().x / 2, (int)m_Window.getSize().y / 2 });
+  p.unitTests();
+  cout << "Unit tests complete.  Starting engine..." << endl;
+
+  while(m_Window.isOpen)
+    {
+      Time time2 = clock.restart();
+      input();
+      update(time1);
+      draw();
+    }
 }

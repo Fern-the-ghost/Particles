7970f363805a4817290eb38558e48c6890db9e32
Fern-the-ghost <W1907532@apps.losrios.edu>
2025-05-04 17:34:59 -0700

    Update Engine.cpp

--- code/Engine.cpp
+++ code/Engine.cpp
@@ -5,10 +5,12 @@ using namespace sf;
 using namespace std;
 
 RenderWindow m_Window;
+vector<Particle> m_particles;
 
 Engine::Engine()
 {
-  
+  create(m_Window);
+  VideoMode::getDesktopMode();
 }
 
 void input()

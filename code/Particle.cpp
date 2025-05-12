82f1b92e4cb61e4d3730ecbb79388e7f64dd5909
Fern-the-ghost <W1907532@apps.losrios.edu>
2025-05-12 11:48:48 -0700

    Update Particle.cpp

--- code/Particle.cpp
+++ code/Particle.cpp
@@ -129,7 +129,7 @@ void Particle::draw(RenderTarget& target, RenderStates states) const {
     lines[0].color = m_color1;
 
     for (int j = 1; j <= m_numPoints; j++) {
-        lines[j].position = target.mapCoordsToPixel(Vector2f(m_A(0, j - 1), m_A(1, j - 1)), m_cartesianPlane);
+        lines[j].position = (Vector2f)target.mapCoordsToPixel(Vector2f(m_A(0, j - 1), m_A(1, j - 1)), m_cartesianPlane);
         lines[j].color = m_color2;
     }
     target.draw(lines, states);
